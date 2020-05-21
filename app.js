2f0fe744054f0b3ed2b714ecc3efa548bc51b759
ceress <markmesina69@gmail.com>
2020-05-21 15:50:46 -0700

    added rotate func

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if(e.keyCode === 37) {
       moveLeft();
     } else if (e.keyCode === 38) {
-      //rotate()
+      rotate()
     } else if (e.keyCode === 39) {
       moveRight()
     } else if (e.keyCode === 40) {
@@ -131,9 +131,20 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     draw()
-
   }
 
+  //rotate the teromino
+  function rotate() {
+    undraw()
+    currentRotation ++
+    if(currentRotation === current.length) {
+      currentRotation = 0
+    }
+    current = theTetrominoes[random] [currentRotation]
+    draw()
+  }
+
+  
 
 
 
