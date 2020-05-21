f501e1628832994819bb0306d95feb114211a2a4
ceress <markmesina69@gmail.com>
2020-05-21 16:12:22 -0700

    create upNext block and added functionality

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelectorAll('#score');
   const StartBtn = document.querySelector('#start-button');
   const width = 10
-
+  let nextRandom = 0
   //tetrominoes
   const lTetromino = [
     [1, width+1, width*2+1, 2],
@@ -97,10 +97,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
       current.forEach(index => squares[ currentPosition + index].classList.add('taken'))
       //start a new tetromino falling
-      random = Math.floor(Math.random() * theTetrominoes.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random] [currentRotation]
       currentPosition = 4
       draw()
+      displayShape()
     }
   }
 
@@ -144,9 +146,29 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
+  //show up next tetromino
+
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  let displayIndex = 0
   
-
-
+  //the tetrominos w/o rotatio
+  const upNextTetrominoes = [
+    [1,displayWidth+1, displayWidth*2+1, 2],
+    [0, displayWidth, displayWidth+1, displayWidth*2+1],
+    [1, displayWidth, displayWidth+1, displayWidth+2],
+    [0, 1, displayWidth, displayWidth+1],
+    [1,displayWidth+1, displayWidth*2+1, displayWidth*3+1]
+  ]
+  //display shape
+  function displayShape() {
+    displaySquares.forEach( square => {
+      square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach ( index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+  }
 
 
 
